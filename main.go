ad0569aafeb1caf781c64dd725d3d6bc2581c1e4
Mohamed Tawfik <motawfik98@outlook.com>
2021-12-14 19:38:37 +0200

    add CustomValidator to validate struct fields

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"chat-system/api"
 	"chat-system/database"
 	"chat-system/service"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	e.Validator = &api.CustomValidator{Validator: validator.New()}
 
 	db, err := database.InitDB()
 	if err != nil {
